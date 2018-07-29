2c643af9095ef0cb7528f209673fb52583bbac7c
persi <perminov-s-i@yandex.ru>
2018-07-29 18:07:48 +0500

    playlist validation; playlist tests

--- root/ideezer/models.py
+++ root/ideezer/models.py
@@ -3,6 +3,11 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 
+ITUNES = 'itunes'
+DEEZER = 'deezer'
+source_names = {ITUNES: 'iTunes', DEEZER: 'Deezer'}
+
+
 class BaseTrack(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
@@ -80,27 +85,88 @@ class Playlist(models.Model, UserFilterModelMixin):
     deezer_title = models.CharField(max_length=255, null=True, blank=True)
     deezer_content = models.ManyToManyField(DeezerTrack, blank=True)
 
+    class Meta:
+        unique_together = (
+            ('user', 'itunes_id')
+        )
+
     def get_absolute_url(self):
         return reverse('playlist_detail', args=[self.id])
 
     def __str__(self):
-        return ' | '.join(part for part in (
-            self.str_itunes(), self.str_deezer()
-        ) if part)
+        return ' | '.join(
+            part for part in (self.str_itunes, self.str_deezer) if part
+        )
 
+    @property
     def str_itunes(self):
-        if self.itunes_title:
-            return '{} ({} iTunes tracks) ({})'.format(
-                self.itunes_title, self.itunes_content.count(),
-                self.user,
-            )
+        """ <title> (iTunes <track_cnt>) (<owner_name>) """
+        return self.__str(source=ITUNES, with_user=True)
 
+    @property
     def str_deezer(self):
-        if self.deezer_title:
-            return '{} ({} Deezer tracks)'.format(
-                self.deezer_title, self.deezer_content.count(),
-            )
+        """ <title> (Deezer <track_cnt>) """
+        return self.__str(source=DEEZER, with_user=False)
 
-    # TODO validation while save:
-    # `itunes_title` and `deezer_title` cannot be NULL together
-    # `{}_title` cannot be NULL if `{}_id` IS NOT NULL
+    def __str(self, source, with_user=False):
+        """ Returns part of playlist string representation
+
+        :param source: 'itunes' or 'deezer'
+        :param with_user: add playlist owner name to result?
+        """
+        assert source in (ITUNES, DEEZER)
+
+        _title = getattr(self, source + '_title')
+        if not _title:
+            return
+        _id = getattr(self, source + '_id')
+        _content = getattr(self, source + '_content')
+
+        count_section = ''
+        if self.id:  # can we use many-to-many relationship?
+            count_section = ' ({} {} tracks)'.format(
+                _content.count(), source_names[source]
+            )
+        user_section = ' ({})'.format(self.user) if with_user else ''
+        return '{}{}{}'.format(_title, count_section, user_section)
+
+    def save(self, *args, **kwargs):
+        err = self.validate()
+        if err:
+            raise ValueError(err)
+
+        if self.id:  # can we use many-to-many relationship?
+            # Check playlist contains only tracks from playlist.user
+            # Does not work, when saving from admin interface
+            for itunes_track in self.itunes_content.all():
+                if itunes_track.user != self.user:
+                    err = 'Playlist tracks must belongs to playlist User'
+                    raise ValueError(err)
+        return super(Playlist, self).save(*args, **kwargs)
+
+    def validate(self):
+        """ 'Interface' validation. Check playlist attribute is correct
+
+        :return error text if incorrect, None otherwise
+        """
+        # If iTunes/Deezer id is filled, its title must be filled too.
+        for attr1, attr2, name1, name2, in (
+            (self.itunes_id, self.itunes_title, 'itunes_id', 'itunes_title'),
+            (self.deezer_id, self.deezer_title, 'deezer_id', 'deezer_title'),
+        ):
+            err = self.validate_xor(attr1, attr2, name1, name2)
+            if err:
+                return err
+
+        if not self.itunes_title and not self.deezer_title:
+            return 'Playlist title cannot be None'
+
+    @staticmethod
+    def validate_xor(attr1, attr2, name1, name2):
+        if bool(attr1) != bool(attr2):
+            return (
+                '{name1} and {name2} attributes must be `None` or `not None` '
+                'together. Got {name1} = `{value1}` and {name2} = `{value2}` '
+                'instead'.format(
+                    name1=name1, name2=name2, value1=attr1, value2=attr2,
+                ))
--- root/ideezer/tests.py
+++ root/ideezer/tests.py
@@ -1,3 +1,83 @@
+from random import randint
+import uuid
+
 from django.test import TestCase
 
-# Create your tests here.
+from . import models as md
+
+
+def strand(length=None):
+    return uuid.uuid4().hex[:length]
+
+
+def intrand(minimum=0, maximum=1000000):
+    return randint(a=minimum, b=maximum)
+
+
+def create_user_track(user):
+    return md.UserTrack.objects.create(
+        itunes_id=intrand(), title=strand(), artist=strand(), user=user
+    )
+
+
+def create_playlist(user):
+    return md.Playlist.objects.create(
+        itunes_title=strand(), itunes_id=intrand(), user=user
+    )
+
+
+class PlaylistTestCase(TestCase):
+    def setUp(self):
+        self.user1 = md.User.objects.create_user(strand())
+        self.user2 = md.User.objects.create_user(strand())
+        # tracks belong to one user
+        self.consistent_tracks = [
+            create_user_track(self.user1), create_user_track(self.user1)
+        ]
+        # tracks belong to defferent users
+        self.unconsistent_tracks = [
+            create_user_track(self.user1), create_user_track(self.user2)
+        ]
+
+    # #######################
+    # Not NULL attribute test
+    # #######################
+
+    def test_no_title_err(self):
+        with self.assertRaisesMessage(
+            ValueError, 'Playlist title cannot be None'
+        ):
+            md.Playlist.objects.create(user=self.user1)
+
+    def test_consistency_err_itunes1(self):
+        with self.assertRaises(ValueError):
+            md.Playlist.objects.create(itunes_id=intrand(), user=self.user1)
+
+    def test_consistency_err_itunes2(self):
+        with self.assertRaises(ValueError):
+            md.Playlist.objects.create(itunes_title=strand(), user=self.user1)
+
+    def test_consistency_err_deezer1(self):
+        with self.assertRaises(ValueError):
+            md.Playlist.objects.create(deezer_id=intrand(), user=self.user1)
+
+    def test_consistency_err_deezer2(self):
+        with self.assertRaises(ValueError):
+            md.Playlist.objects.create(deezer_title=strand(), user=self.user1)
+
+    # #######################
+    # Playlist content tests
+    # #######################
+
+    def test_content_ok(self):
+        plst = create_playlist(user=self.user1)
+        plst.itunes_content.add(*[track for track in self.consistent_tracks])
+        plst.save()
+
+    def test_content_err(self):
+        plst = create_playlist(user=self.user1)
+        plst.itunes_content.add(*[track for track in self.unconsistent_tracks])
+        with self.assertRaisesMessage(
+            ValueError, 'Playlist tracks must belongs to playlist User'
+        ):
+            plst.save()
